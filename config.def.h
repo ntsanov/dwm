64659a7485e851bcb89c27a9aa34eb8c6d939912
ntsanov <tsanov86@gmail.com>
2020-09-17 16:55:24 +0200

    Applied dwm-swallow

--- config.def.h
+++ config.def.h
@@ -33,10 +33,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     	NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",  	NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Ghidra",  	NULL,       NULL,       0,       	  0,           -1 },
+	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           0,         0,        -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
+	{ "st",       NULL,       NULL,       0,            0,           1,         1,        -1 },
 };
 
 // GIST - https://gist.github.com/palopezv/efd34059af6126ad970940bcc6a90f2e
